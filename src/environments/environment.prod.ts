818c193eee30d17b7c6cff7a3923df5bf7de16b1
AgustinAlfonso <44329975+AgustinAlfonso@users.noreply.github.com>
2025-12-05 21:28:07 -0300

    Update backend url (#5)
    * Update backend URL to https://cheque-defined-diesel-these.trycloudflare.com
    
    * Update backend URL to https://declined-feeding-elsewhere-expects.trycloudflare.com
    
    * Increase CSS budget limit to 6kB to fix warning
    
    * Update backend URL to https://hosting-nikon-phd-sociology.trycloudflare.com
    
    * Update backend URL to valuation-releases-democratic-useful.trycloudflare.com
    
    ---------
    
    Co-authored-by: AgustinAlfonso <aalfonso@todo1.net>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://hosting-nikon-phd-sociology.trycloudflare.com'
+  apiBaseUrl: 'https://valuation-releases-democratic-useful.trycloudflare.com'
 };
 
